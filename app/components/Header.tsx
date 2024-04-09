d4b6f30643f4de4e3e6a342fd68baf53a82d159d
oscarmauriciogiraldo <oscardan@utp.edu.co>
2024-04-08 22:52:11 -0500

    new header style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
                 </div>
             </div>
-            <div className="searchContainer">
+            <div className={styles.searchContainer}>
 
             </div>
         </header>
